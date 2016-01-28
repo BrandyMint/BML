103ec1226f324ce9bab8dc1f6f622a86fb1364a5
Danil Pismenny <danil@brandymint.ru>
2016-01-28 20:33:17 +0300

    minor

--- app/services/yandex_direct/ad_groups.rb
+++ app/services/yandex_direct/ad_groups.rb
@@ -1,6 +1,9 @@
 module YandexDirect
   class AdGroups < BaseResource
-    FIELD_NAMES = %w(Id CampaignId Status Name RegionIds NegativeKeywords TrackingParams Type)
+    FIELD_NAMES       = %w(Id CampaignId Status Name RegionIds NegativeKeywords TrackingParams Type)
+    TYPES             = %w(TEXT_AD_GROUP MOBILE_APP_AD_GROUP DYNAMIC_TEXT_AD_GROUP)
+    STATUSES          = %w(ACCEPTED DRAFT MODERATION PREACCEPTED REJECTED)
+    APP_ICON_STATUSES = %w(ACCEPTED MODERATION REJECTED)
 
     def get(fieldNames: FIELD_NAMES, selectionCriteria: {})
       make_response(
