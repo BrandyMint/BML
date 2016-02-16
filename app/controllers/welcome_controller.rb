387c5c667287aa6272627dc93278be2f7b0cd97d
Danil Pismenny <danil@brandymint.ru>
2016-02-16 12:32:27 +0300

    redirect to bm

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    redirect_to 'http://molodost.bz' if request.domain.ends_with? 'bmland.ru'
+    # redirect_to 'http://molodost.bz' if request.domain.ends_with? 'bmland.ru'
+    redirect_to 'http://molodost.bz'
   end
 end
