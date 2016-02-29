65eb60f0f036f5f3da22106c3d958da174ecb663
Danil Pismenny <danil@brandymint.ru>
2016-02-29 12:36:10 +0300

    fix tld_length

--- lib/api_constraint.rb
+++ lib/api_constraint.rb
@@ -2,7 +2,7 @@ class ApiConstraint
   SUBDOMAIN = 'api'
 
   def self.matches?(request)
-    Rails.logger.warn("ApiConstraint #{request.domain} / #{request.subdomain},#{SUBDOMAIN}, #{request.subdomain == SUBDOMAIN}")
+    Rails.logger.warn("ApiConstraint (#{request.tld_length}) '#{request.domain}' / #{request.subdomain},#{SUBDOMAIN}, #{request.subdomain == SUBDOMAIN}")
     request.subdomain == SUBDOMAIN
   end
 end
