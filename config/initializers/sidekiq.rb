e200c286082f0d0e5ed4aed139e928fa075e70d7
Danil Pismenny <danil@brandymint.ru>
2016-03-11 22:00:54 +0300

    fix sidekiq for teamcity

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,7 +5,7 @@ require 'sidekiq-status'
 if Rails.env.production?
   Sidekiq.configure_server do |config|
     # Sidekiq::Logging.logger = LoggerCreator.build :sidekiq
-    config.redis = Secrets.sidekiq.redis.symbolize_keys
+    config.redis = Secrets.sidekiq.redis.symbolize_keys if Secrets.sidekiq.try(:redis).present?
     config.error_handlers << proc { |ex, context| Bugsnag.notify(ex, context) }
     config.failures_max_count = 50_000
     config.failures_default_mode = :exhausted
