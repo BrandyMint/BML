f32c6b0bcd59e30ddfdc47c546f3292126221ef6
Danil Pismenny <danil@brandymint.ru>
2016-03-13 10:54:39 +0300

    fix spec

--- spec/commands/create_lead_spec.rb
+++ spec/commands/create_lead_spec.rb
@@ -1,27 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe CreateLead do
-  let(:params) { {} }
+  let(:data) { {} }
   let(:cookies) { {} }
   let(:viewer_uid) { 'xxx' }
+  let(:variant) { nil }
 
-  subject { described_class.new(params: params, cookies: cookies, viewer_uid: viewer_uid) }
+  subject do
+    described_class.new(data: data,
+                        variant: variant,
+                        cookies: cookies,
+                        viewer_uid: viewer_uid)
+  end
 
   before do
     Viewer.touch_or_create viewer_uid
   end
 
   describe '#call' do
-    context 'no landing' do
-      it { expect { subject.call }.to raise_error ActiveRecord::RecordNotFound }
-    end
-
     context 'valid params' do
       let(:variant) { create :variant }
-      let(:params) { { variant_uuid: variant.uuid, name: 'John Doe' } }
+      let(:data) { { name: 'John Doe' } }
+      let(:utm_source) { '123' }
       let(:cookies) do
         {
-          first_utm_source: '123',
+          first_utm_source: utm_source,
           last_utm_source: '111'
         }
       end
@@ -30,7 +33,7 @@ RSpec.describe CreateLead do
         subject.call
       end
       it 'must create lead' do
-        expect(variant.leads.last.first_utm_source).to eq '123'
+        expect(variant.leads.last.first_utm_source).to eq utm_source
         expect(variant.leads.last.viewer).to be_a Viewer
       end
     end
