3a45db5a289b9f5c025be6e27c1746b42649e54e
Danil Pismenny <danil@brandymint.ru>
2016-03-16 11:26:16 +0300

    clear_path в Landing

--- app/models/concerns/landing_path.rb
+++ app/models/concerns/landing_path.rb
@@ -2,7 +2,9 @@ module LandingPath
   extend ActiveSupport::Concern
 
   included do
+    before_validation :clear_path
     before_validation :generate_path
+    before_save :clear_path
     validates :path,
               uniqueness: true,
               uri_component: { component: :ABS_PATH },
@@ -10,6 +12,15 @@ module LandingPath
   end
 
   private
+
+  def clear_path
+    if self.path.blank?
+      self.path = '/'
+    end
+    if self.path.length > 1
+      self.path = self.path.slice(0, self.path.length-1) if self.path.end_with? '/'
+    end
+  end
 
   def generate_path
     self.path ||= '/' + SecureRandom.hex(4)
