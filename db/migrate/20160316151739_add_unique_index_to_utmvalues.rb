fac3d73115f1b881426b4ab1210af50dd6d7151e
Danil Pismenny <danil@brandymint.ru>
2016-03-16 18:34:19 +0300

    remove redundant migration

--- db/migrate/20160316151739_add_unique_index_to_utmvalues.rb
+++
@@ -1,6 +0,0 @@
-class AddUniqueIndexToUtmvalues < ActiveRecord::Migration
-  def change
-    UtmValue.delete_all
-    add_index :utm_values, [:landing_id, :key_type, :value], unique: true
-  end
-end
