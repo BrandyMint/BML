a98547b53898d1337b34cd7bb610fc0e959dab9c
Denis Khabarov <admin@saymon21-root.pro>
2016-03-17 12:20:06 +0400

    Линкуем папочку для letsencrypt

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :git_enable_submodules, 1
 set :keep_releases, 10
 
 set :linked_files, %w(config/database.yml config/secrets.yml)
-set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets public/uploads)
+set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets public/uploads public/.well-known)
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
