542a537e4dba70e14973f9a1e38a2a334d667c4f
Danil Pismenny <danil@brandymint.ru>
2016-03-23 15:36:05 +0300

    api_key для текстов

--- app/models/concerns/account_access_key.rb
+++ app/models/concerns/account_access_key.rb
@@ -1,9 +1,21 @@
 module AccountAccessKey
   extend ActiveSupport::Concern
 
+  TEST_ACCESS_KEY = 'test';
+  TEST_ACCOUNT_ID = 2;
+
   included do
     before_create :generate_access_key
+
+    def self.find_by_access_key(access_key)
+      if Rails.env.development? && access_key == TEST_ACCESS_KEY
+        Account.find TEST_ACCOUNT_ID
+      else
+        super access_key
+      end
+    end
   end
+
 
   private
 
