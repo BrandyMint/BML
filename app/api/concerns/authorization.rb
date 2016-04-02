c3431a23c1b287d7adec35f7912ca5ad4cf2dbc3
Danil Pismenny <danil@brandymint.ru>
2016-04-02 03:36:39 +0300

    Bump v0.10.55 fix validation

--- app/api/concerns/authorization.rb
+++ app/api/concerns/authorization.rb
@@ -1,9 +1,22 @@
 module Authorization
   extend ActiveSupport::Concern
 
+  TEST_ACCESS_KEY = 'test'.freeze
+  TEST_ACCOUNT_ID = 2
+
   included do
     helpers do
+      def test?
+        Rails.env.development? && api_key == TEST_ACCESS_KEY
+      end
+
+      def find_test_account
+        Variant.find_by_uuid(params[:uuid]).try(:account) ||
+          Account.find(TEST_ACCOUNT_ID)
+      end
+
       def current_account
+        return find_test_account if test?
         @_current_account ||= Account.find_by_access_key api_key
       end
 
