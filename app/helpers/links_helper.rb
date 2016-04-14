3408987e1c471243cbf90e7a5bfead5e0f8f2f42
Danil Pismenny <danil@brandymint.ru>
2016-04-14 22:10:56 +0300

    rubocop

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -2,9 +2,9 @@ module LinksHelper
   def column_hide_button(column)
     return unless column.persisted?
     link_to hide_landing_collection_column_path(current_landing, current_collection, column),
-      method: :patch,
-      title: 'Скрыть колонку',
-      tooltip: true do
+            method: :patch,
+            title: 'Скрыть колонку',
+            tooltip: true do
       fa_icon 'eye-slash'
     end
   end
@@ -12,6 +12,7 @@ module LinksHelper
   def delete_site_button(landing)
     link_to 'Удалить сайт', landing_url(landing), class: 'btn btn-danger', method: :delete, data: { confirm: 'Удалить сайт на всегда?' }
   end
+
   def columns_icon(collection)
     link_to landing_collection_columns_path(current_landing, collection), title: 'Колонки', tooltip: true do
       fa_icon :columns
