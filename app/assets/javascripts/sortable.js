04d63fa73b73adc82bec2924a0ea04e54e548fc1
Danil Pismenny <danil@brandymint.ru>
2016-04-15 11:06:13 +0300

    fix сортировки

--- app/assets/javascripts/sortable.js
+++ app/assets/javascripts/sortable.js
@@ -1,41 +1,52 @@
 'use strict';
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+function onStart(ref) {
+  var item = ref.item;
 
-function onStart(_ref) {
-  var item = _ref.item;
-
+  console.log('onStart', ref.oldIndex);
+  // TODO Актуально только для таблицы?
   $(item).children().each(function (index) {
     $(this).width($(this).width());
   });
 }
 
-function onUpdate(_ref2) {
-  var item = _ref2.item;
-  var newIndex = _ref2.newIndex;
-  var oldIndex = _ref2.oldIndex;
+function onEnd(ref) {
+  var item = ref.item;
 
+  console.log('onEnd', ref.oldIndex, ref.newIndex);
+}
+
+function onUpdate(ref) {
+  var item = ref.item;
+  var newIndex = ref.newIndex;
+  var oldIndex = ref.oldIndex;
+
+  console.log('onUpdate', ref.oldIndex, ref.newIndex);
   var $item = $(item).find('[data-sort-url]');
   var url = $item.data('sort-url');
 
   if (oldIndex !== newIndex && url) {
     (function () {
       var start = $item.data('sort-start') || 0;
-      var method = $item.data('sort-post') || 'POST';
+      var method = $item.data('sort-method') || 'POST';
       var position = $item.data('sort-position') || newIndex + 1;
-      var onSuccess = $item.data('sort-success-action');
-      var customParams = $item.data('sort-custom-params') || {};
+      var successAction = $item.data('sort-success-action');
+      var data = $item.data('sort-custom-params') || {};
 
-      if (typeof customParams === 'string') {
-        customParams = JSON.parse(customParams);
+      if (typeof data === 'string') {
+        data = JSON.parse(data);
       }
+
+      data['position'] = position;
+      data['start'] = start;
 
       $.ajax({
         url: url,
         method: method,
-        data: _extends({}, customParams, { position: position, start: start })
+        data: data,
       }).then(function () {
-        if (onSuccess !== 'nothing') {
+        if (successAction == 'reload') {
+          // TODO Turbolinks?
           window.location.reload();
         }
       }).fail(function () {
@@ -45,53 +56,30 @@ function onUpdate(_ref2) {
   }
 }
 
-$.fn.sortableJS = function (options) {
-  var retVal;
-  var args = arguments;
-
-  this.each(function () {
-    var $el = $(this);
-    var sortable = $el.data('sortable');
-
-    if (!sortable && (options instanceof Object || !options)) {
-      sortable = new Sortable(this, options);
-      $el.data('sortable', sortable);
-    }
-
-    if (sortable) {
-      if (options === 'widget') {
-        return sortable;
-      } else if (options === 'destroy') {
-        sortable.destroy();
-        $el.removeData('sortable');
-      } else if (typeof sortable[options] === 'function') {
-        retVal = sortable[options].apply(sortable, [].slice.call(args, 1));
-      } else if (options in sortable.options) {
-        retVal = sortable.option.apply(sortable, args);
-      }
-    }
-  });
-
-  return retVal === void 0 ? this : retVal;
+var OPTIONS = {
+  forceFallback: true,
+  // draggable: 'div',
+  // ghostClass: 'sortable-ghost',
+  ghostClass: 'row--highlight',
+  onStart: onStart,
+  onUpdate: onUpdate,
+  onEnd: onEnd,
+  handle: '.handle',
+  // draggable: '[table-sortable]',
+  draggable: 'tr',
+  animation: 150,
+  forceFallback: false // С форсажем глючит на Safari: в onUpdate newIndex и oldIndex одинаковые
 };
 
-$.fn.accountSortable = function (options) {
-  this.sortableJS(_extends({
-    onStart: onStart,
-    onUpdate: onUpdate,
-    ghostClass: 'sortable-ghost',
-    animation: 150,
-    forceFallback: true
-  }, options));
+var attachSortable = function() {
+  var $el = $(this);
+  if ($el.data('sortable-inited')) {
+    return;
+  }
+  Sortable.create(this, OPTIONS);
+  $el.data('sortable-inited', true);
 };
 
-$.fn.accountTableSortable = function (options) {
-  this.sortableJS(_extends({
-    onStart: onStart,
-    onUpdate: onUpdate,
-    handle: '.handle',
-    ghostClass: 'sortable-ghost',
-    animation: 150,
-    forceFallback: true
-  }, options));
-};
+$(document).on('ready page:load', function() {
+  $('[table-sortable]').each( attachSortable );
+});
