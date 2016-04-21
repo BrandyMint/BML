f85b926199b96b14d4d001497703991a29b2d59e
Danil Pismenny <danil@brandymint.ru>
2016-04-21 11:42:44 +0300

    parse leadboard score

--- app/services/leader_board/builder.rb
+++ app/services/leader_board/builder.rb
@@ -33,7 +33,7 @@ module LeaderBoard
     UNKNOWN_TITLE     = 'Аноним'.freeze
 
     DEFAULT_EVENT     = 'DefaultEvent'.freeze
-    DEFAULT_division = 'DefaultDivision'.freeze
+    DEFAULT_DIVISION  = 'DefaultDivision'.freeze
 
     def add_division(division)
       @divisions.push(division)
@@ -46,7 +46,7 @@ module LeaderBoard
     def build_results
       @results = {}
       leads.find_each do |lead|
-        division = lead.data[DIVISION_COLUMN] || DEFAULT_division
+        division = lead.data[DIVISION_COLUMN] || DEFAULT_DIVISION
         add_division division
 
         event = lead.data[EVENT_COLUMN] || DEFAULT_EVENT
@@ -57,12 +57,17 @@ module LeaderBoard
 
         note = lead.data[NOTE_COLUMN]
         title = lead.data.slice(*TITLE_COLUMNS).values.join(' ') || UNKNOWN_TITLE
-        score = lead.data[SCORE_COLUMN].to_i
+        score = parse_score lead.data[SCORE_COLUMN]
         score = nil if score == 0
 
         ranks_table = find_ranks_table(division, event, sex)
         add_rank ranks_table[:records], title, score, note
       end
+    end
+
+    def parse_score(score)
+      score = score.tr ',', '.' if score.is_a? String
+      score.to_f
     end
 
     def result_key(*categories)
