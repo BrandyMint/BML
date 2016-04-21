9cf0a02a6b424e307110da8841a98606020d8afe
Danil Pismenny <danil@brandymint.ru>
2016-04-22 00:01:15 +0300

    Lead#delete_data_key!

--- spec/models/lead_spec.rb
+++ spec/models/lead_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Lead, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:lead) { create :lead, data: { a: '1' } }
+
+  it { expect(lead.data.a).to eq '1' }
+
+  context '#delete_data_key' do
+    before do
+      lead.delete_data_key! :a
+    end
+
+    it { expect(lead.data.keys).to be_empty }
+  end
 end
