475905101704dff401883e4a7d597fd362327cfb
Danil Pismenny <danil@brandymint.ru>
2016-04-22 15:54:09 +0300

    minor

--- app/helpers/leads_helper.rb
+++ app/helpers/leads_helper.rb
@@ -7,8 +7,10 @@ module LeadsHelper
   def lead_additinal_info(lead)
     if lead.is_a? Client
       link_to I18n.t(:items_count, count: lead.leads.count), landing_leads_url(current_landing, state: :any, client_id: lead.id)
-    else
+    elsif lead.client.present?
       link_to lead.client, landing_lead_url(current_landing, lead.client)
+    else
+      empty_value_html
     end
   end
 end
