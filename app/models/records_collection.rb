7bab068755bc1a5f6d214389f1cc7a3329fe0b4d
Danil Pismenny <danil@brandymint.ru>
2016-04-22 16:34:55 +0300

    fix builder

--- app/models/records_collection.rb
+++ app/models/records_collection.rb
@@ -1,5 +1,5 @@
 class RecordsCollection < Collection
-  has_many :leads, dependent: :delete_all
+  has_many :leads, dependent: :delete_all, foreign_key: :collection_id
 
   def self.model_name
     superclass.model_name
